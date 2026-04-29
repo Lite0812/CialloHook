5ea6dc7c5d0342444449ab355c3078d9c6ecf94e
Lite0812 <2155514215@qq.com>
2026-04-29 16:30:13 +0800

    删除错误提交

--- CialloLauncher/config/LauncherConfig.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../LauncherTypes.h"
-
-namespace CialloLauncher
-{
-	bool LoadLauncherConfig(const std::wstring& exeDir, const std::wstring& exeNameNoExt, LauncherConfig& config);
-}
